7b5ab20a8838ffd79186927a7bac79ffe70d9687
NikosMouzakitis <mzktsn@gmail.com>
2016-02-25 00:34:24 +0200

    Create build_one_two_three.c

---
+++ build_one_two_three.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+        int data;
+        struct node* next;
+} ;
+
+struct node * buildOneTwoThree()
+{
+        struct node * head;
+        struct node * second;
+        struct node * third;
+
+        head = (struct node *)malloc(sizeof(struct node));
+        second = (struct node *)malloc(sizeof(struct node));
+        third = (struct node *)malloc(sizeof(struct node));
+
+
+        head->data = 1;
+        head->next = second;
+
+        second->data = 2;
+        second->next = third;
+
+        third->data = 3;
+        third->next = NULL;
+
+
+        return head;
+}
+
+
+int main(void)
+{
+        struct node *ptr;
+        ptr = buildOneTwoThree();
+
+        while(ptr->next != NULL)
+        {
+                printf("%d ",ptr->data);
+                ptr = ptr->next;
+        }
+
+        printf("%d ",ptr->data);
+
+        return 0;
+}
+
